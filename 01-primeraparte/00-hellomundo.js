3f1fe4fa2c189ec3f405fc7b774a8c84a86dc7c4
ArturoFC1 <arthurcode420@gmail.com>
2025-04-17 22:24:17 -0600

    Este comit realiza la insercion de la primera parte de los codigos

---
+++ 01-primeraparte/00-hellomundo.js
@@ -0,0 +1,5 @@
+//Este es un comentario simple
+console.log("Hola mundo");
+console.log("Hola mundo");
+console.log(5);
+console.log(5+5);                                                                                                         
---
+++ 01-primeraparte/01-variables.js
@@ -0,0 +1,19 @@
+/**
+ * Algunas de la variables de JavaScript son las siguientes
+ */
+//VAR                  Este tipo de variable ya no se recomienda ya que se puede alterar facil la variable
+var perroSalvaje = "Coki";
+console.log(perroSalvaje);
+
+perroSalvaje = "Negro";
+console.log(perroSalvaje);
+//LET                  Este tipo de variable es recomendado, se puede alterar la variable pero solo dentro del mismo bloque
+let perroTierno = "Kiara";
+console.log(perroTierno);
+
+perroTierno = "Cafe";
+console.log(perroTierno);
+//CONST                Este tipo de varibale no puede alterar la variable
+const perroAgresivo = "peluchin";
+console.log(perroAgresivo);
+
